44457bfa71eaa89325517c44ad9679f67d8eed4d
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-07-15 16:10:09 +0200

    added temporary layout

---
+++ js/script.js
@@ -0,0 +1,30 @@
+console.log('VUE OK', Vue);
+
+const boolzApp = new Vue({
+    el: '#root',
+    data: {
+        user: {
+            name: 'Nome Utente',
+            avatar: '_io'
+        },
+        contacts: [
+            {
+                name: 'Michele',
+                avatar: '_1',
+            },
+            {
+                name: 'Fabio',
+                avatar: '_2',
+            },
+            {
+                name: 'Samuele',
+                avatar: '_3',
+            },
+            {
+                name: 'Luisa',
+                avatar: '_4',
+            },
+        ]
+    },
+
+});
